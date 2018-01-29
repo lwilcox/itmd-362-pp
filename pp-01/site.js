c149ee6c885413eb94d5b9c4921ae9bbc5e767c5
Logan Wilcox <lwilcox@hawk.iit.edu>
2018-01-28 20:39:22 -0600

    removed alert and changed to append

--- pp-01/site.js
+++ pp-01/site.js
@@ -9,25 +9,32 @@ $('#contact-form').on('submit',
     var agecheck = check.getYear();
     var check2 = /\d{10}/;
     var check3 = /.*@.*/;
-    e.preventDefault();
+
     if (!(birthday > agecheck)) {
       console.log('must be 18 years or older');
-      alert('You must be 18 years or older');
+      $('#birthday-input').remove('#warning');
+      $('#birthday-input').append('<li id="warning">must be 18 years or older</li>');
+      //alert('You must be 18 years or older');
       return false;
     }
     if(!(check2.test(phone)))
     {
       console.log('invalid phone number');
-      alert('Please enter a valid telephone number');
+      //alert('Please enter a valid telephone number');
+      $('#warning').remove;
+      $('#phone-input').append('<li id="warning">Please enter a valid telephone number</li>');
       return false;
     }
     if(!(check3.test(email)))
     {
       console.log('invalid email address');
-      alert('Please enter a valid email address');
+      //alert('Please enter a valid email address');
+      $('#warning').remove;
+      $('#email-input').append('<li id="warning">Please enter a valid email address</li>');
       return false;
     }
     else {
+      e.preventDefault();
       $(this).remove();
       $('body').append('<h1>Thank You! Your form has been submitted. Email Address: ' + email + '</h1>');
     }
