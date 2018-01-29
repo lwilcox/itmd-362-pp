e9b9e2f859df02ac0b3e427d8528075618f33135
Logan Wilcox <lwilcox@hawk.iit.edu>
2018-01-29 14:46:48 -0600

    fixed all linter errors for css, html, and js files.

--- pp-01/site.js
+++ pp-01/site.js
@@ -5,23 +5,22 @@ $('#contact-form').on('submit',
     var email = $('#email').val();
     var birthday = $('#birthday').val();
     var check = new Date();
-    check = new Date(check.setYear(check.getYear() - 18));
     var agecheck = check.getYear();
     var check2 = /\d{10}/;
     var check3 = /.*@.*/;
+    check = new Date(check.setYear(check.getYear() - 18));
 
-    //Not sure whats wrong with my birthday validation but it's not working... 
+
+    //  Not sure whats wrong with my birthday validation but it's not working...
     if (!(birthday > agecheck)) {
       console.log('must be 18 years or older');
       $('#birthday-input').remove('#warning');
       $('#birthday-input').append('<li id="warning">must be 18 years or older</li>');
-      //alert('You must be 18 years or older');
       return false;
     }
     if(!(check2.test(phone)))
     {
       console.log('invalid phone number');
-      //alert('Please enter a valid telephone number');
       $('#warning').remove;
       $('#phone-input').append('<li id="warning">Please enter a valid telephone number</li>');
       return false;
@@ -29,7 +28,6 @@ $('#contact-form').on('submit',
     if(!(check3.test(email)))
     {
       console.log('invalid email address');
-      //alert('Please enter a valid email address');
       $('#warning').remove;
       $('#email-input').append('<li id="warning">Please enter a valid email address</li>');
       return false;
